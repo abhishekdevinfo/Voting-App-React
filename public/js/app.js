023a17b26addfffe964eff27bbb0da0eaa079a13
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-05-11 12:10:04 +0530

    React component, Babel I wrote first react component. And transcompile es6 code to es5 using babel. Finally i used ReactDOM.render() to define which component i want to render and where to render in dom.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable items">
+        Hello, Friends! Its a basic react component.
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
